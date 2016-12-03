106ffa605c25feb24536ee9d6f7ebecc6de8154c
Brendan <bring0@icloud.com>
2016-12-03 02:13:58 -0600

    asdf

---
+++ ContosoUniversityData/ClassLibrary1/Class1.cs
@@ -0,0 +1,52 @@
+﻿using ContosoSite.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace ClassLibrary1
+{
+    public static class Generator
+    {
+        private static Assembly _assembly = Assembly.GetExecutingAssembly();
+        private static Stream _imageStream;
+        private static StreamReader _textStreamReader;
+        private static Random rand = new Random();
+
+        private static List<string> FirstNameList =
+            _enumerateLines(new StreamReader(_assembly.GetManifestResourceStream("ClassLibrary1.first_names.txt")))
+                .ToList();
+
+        private static List<string> LastNameList =
+           _enumerateLines(new StreamReader(_assembly.GetManifestResourceStream("ClassLibrary1.last_names.txt")))
+               .ToList();
+
+        private static IEnumerable<string> _enumerateLines(TextReader reader)
+        {
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                yield return line;
+            }
+        }
+
+        public static int GenRandomInt(Random random, int max)
+        {
+            int rRet = random.Next(1, max);
+            return rRet;
+        }
+
+        public static Student GenerateStudent()
+        {
+            Student rRet = new Student
+            {
+                FirstName = FirstNameList[GenRandomInt(rand, FirstNameList.Count)],
+                LastName = LastNameList[GenRandomInt(rand, LastNameList.Count)],
+            };
+
+            return rRet;
+        }
+    }
+}
---
+++ ContosoUniversityData/ContosoSite/Controllers/StudentsController.cs
@@ -0,0 +1,177 @@
+﻿using ContosoSite.Models;
+using ContosoSite.Properties;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+
+namespace ContosoSite.Controllers
+{
+    public static class Generator
+    {
+        private static string[] firstNames = Regex.Split(Resources.first_names, "\r\n");
+        private static string[] lastNames = Regex.Split(Resources.last_names, "\n");
+        private static Random rand = new Random();
+
+        public static int GenRandomInt(Random random, int max)
+        {
+            int rRet = random.Next(1, max);
+            return rRet;
+        }
+        static string UppercaseFirst(string s)
+        {
+            // Check for empty string.
+            if (string.IsNullOrEmpty(s))
+            {
+                return string.Empty;
+            }
+            // Return char and concat substring.
+            return char.ToUpper(s[0]) + s.Substring(1).ToLower();
+        }
+        public static Student GenerateStudent()
+        {
+
+            Student rRet = new Student
+            {
+                FirstName = UppercaseFirst(firstNames[GenRandomInt(rand, firstNames.Length)]),
+                LastName = UppercaseFirst(lastNames[GenRandomInt(rand, lastNames.Length)]),
+                EnrollmentDate = DateTime.Now
+            };
+
+            return rRet;
+        }
+    }
+
+    public class StudentsController : Controller
+    {
+        private ContosoUniversityDataEntities db = new ContosoUniversityDataEntities();
+
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View(db.Students.ToList());
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Students/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StudentID,LastName,FirstName,EnrollmentDate")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StudentID,LastName,FirstName,EnrollmentDate")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Random()
+        {
+            Student randStudent = Generator.GenerateStudent();
+            //return RedirectToAction("Create", randStudent);
+
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(randStudent);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(randStudent);
+        }
+    }
+}
